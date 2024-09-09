f382d0a1063bc5f955301c0a3b07678eaf1f4246
Jkites <99958936+Jkites@users.noreply.github.com>
2024-09-08 22:11:42 -0700

    added new links to the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
 import logo from './scarf.png';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-center">
       <header className="text-3xl font-bold underline">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="animate-spin h-[16rem]" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Home Page.
         </p>
         <a
-          className="App-link"
+          className="text-[#61dafb]"
           href="https://www.youtube.com/watch?v=WsUmwoVfJLI"
           target="_blank"
           rel="noopener noreferrer"
         >
           Must See
+        </a>
+        <a
+          href="/#"
+          className="pt-20 text-lg font-medium"
+          >
+            <p>
+              “<a className="text-amber-600" href="https://tailwindcss.com/docs/text-color">I </a> 
+              <a className="text-slate-600" href="https://tailwindcss.com/docs/animation">love </a> 
+              <a className="text-red-600" href="https://tailwindcss.com/docs/text-align">Genshin </a> 
+              <a className="text-blue-600"href="https://tailwindcss.com/docs/height"> Impcat </a>
+              .”
+            </p>          
         </a>
       </header>
     </div>
